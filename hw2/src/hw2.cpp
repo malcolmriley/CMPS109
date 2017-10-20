75acd1216deb26c9d122d6992dd111ce24625ae6
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-20 11:58:28 -0700

    Initial commit

---
+++ hw2/src/hw2.cpp
@@ -0,0 +1,11 @@
+//============================================================================
+// Description : Homework 2 for CMPS 109: Dijikstra's Algorithm
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
