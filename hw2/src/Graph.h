020798e186a211f115e0840d35485360bc74ee08
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-20 15:12:19 -0700

    Initial commit

---
+++ hw2/src/Graph.h
@@ -0,0 +1,22 @@
+/*********************************************************************
+ * Graph.h
+ * 
+ * Assignment: hw2
+ *
+ * 10-2017
+ *********************************************************************/
+
+#ifndef GRAPH_H_HW2
+#define GRAPH_H_HW2
+
+#define EDGE_UNDEFINED = -1;
+
+class Graph {
+protected:
+
+public:
+	Graph();
+	virtual ~Graph();
+};
+
+#endif /* GRAPH_H_ */
