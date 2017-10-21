97ba07e4257c46deb1f709c5c2335193c8ffed03
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-20 17:07:42 -0700

    Just include the header

--- hw2/src/hw2.cpp
+++ hw2/src/hw2.cpp
@@ -8,6 +8,7 @@
  * Implementation file for CMPS109 HW2: Dijkstra's Algorithm.
  *********************************************************************/
 #include <iostream>
+#include "Graph.h"
 using namespace std;
 
 int main() {
