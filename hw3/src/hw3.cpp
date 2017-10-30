81b8db06bd404093b22336aee5f1866599eda34f
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-30 13:18:06 -0700

    Initial Commit

---
+++ hw3/src/hw3.cpp
@@ -0,0 +1,17 @@
+/*********************************************************************
+ * hw3.cpp
+ *
+ * Assignment: hw3
+ *
+ * 10-2017
+ *
+ * Implementation file for CMPS109 HW3: Minimum Spanning Tree Algorithm.
+ *********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
