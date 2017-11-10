94d71a069344769c79a536cc6165b04d976e2927
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-10 14:41:32 -0800

    Modify dijkstraPath algorithm to consider cell color

--- hw4/src/hw4.cpp
+++ hw4/src/hw4.cpp
@@ -11,6 +11,7 @@
  *
  *********************************************************************/
 
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <queue>
@@ -22,6 +23,7 @@ using namespace std;
 
 #define DOUBLE_INFINITY std::numeric_limits<double>::max()
 #define SIMPLE_PROMPT "> "
+#define MIN_BOARD_SIZE 2
 
 /* Node Definition */
 struct Cell {
@@ -36,9 +38,46 @@ struct Cell {
 	}
 };
 
+template <typename T>
+void getParameter(string, T*);
+void dijkstraPath(Graph<Cell>*, vector<int>*, char, int, int);
+Graph<Cell> generateBoard(int);
+void populateBoard(Graph<Cell>*);
+void printBoard(Graph<Cell>*);
+
 int main() {
 
+	int boardDimensions = MIN_BOARD_SIZE;
+	int NORTH, SOUTH, EAST, WEST;
+	getParameter("Please enter the dimension of the board:", &boardDimensions);
+	while (boardDimensions < MIN_BOARD_SIZE) {
+		cout << "Minimum board dimension should be greater than two.";
+		getParameter("Please enter the dimension of the board:", &boardDimensions);
+	}
+
 	return 0;
+}
+
+Graph<Cell> generateBoard(int passedDimensions) {
+	cout << "Generating " << passedDimensions << "x" << passedDimensions << " board..." << endl;
+
+	cout << "Board generated!" << endl;
+}
+
+/**
+ * Populates the board with the appropriate edges.
+ */
+void populateBoard(Graph<Cell>* passedBoardGraph, int* passedNorthNode, int* passedSouthNode, int* passedEastNode, int* passedWestNode) {
+	cout << "Executing random moves on board...";
+
+	cout << "Board filled!";
+}
+
+/**
+ * Prints an ASCII representation of the board.
+ */
+void printBoard(Graph<Cell>* passedBoardGraph) {
+
 }
 
 
@@ -59,7 +98,7 @@ void getParameter(string passedString, T* passedType) {
  *
  * Originally implemented for HW2, modified for HW4.
  */
-void dijkstraPath(Graph<Cell>* passedGraph, vector<int>* passedPathVector, int passedStartVertex, int passedEndVertex) {
+void dijkstraPath(Graph<Cell>* passedGraph, vector<int>* passedPathVector, char passedColor, int passedStartVertex, int passedEndVertex) {
 	cout << "Calculating path from vertex " << passedStartVertex << " to vertex " << passedEndVertex << "..." << endl;
 	// Initialize starting nodes
 	priority_queue<int> unvisited = priority_queue<int>();
@@ -82,7 +121,10 @@ void dijkstraPath(Graph<Cell>* passedGraph, vector<int>* passedPathVector, int p
 			if (!visited[ii]) {
 				if (passedGraph->adjacent(currentVertex, ii) && (ii != currentVertex)) {
 					Cell* iteratedNode = passedGraph->getVertex(ii);
-					double traversalWeight = passedGraph->getEdgeWeight(currentVertex, ii) + currentNode->weight;
+					double traversalWeight = DOUBLE_INFINITY; // Consider nodes of different color to have maximum possible traversal weight (untraversable)
+					if (iteratedNode->color == passedColor) {
+						double traversalWeight = passedGraph->getEdgeWeight(currentVertex, ii) + currentNode->weight;
+					}
 					if (!visited[ii]) {
 						unvisited.push(ii);
 					}
@@ -102,4 +144,13 @@ void dijkstraPath(Graph<Cell>* passedGraph, vector<int>* passedPathVector, int p
 		iteratedVertex = passedGraph->getVertex(iteratedVertex)->predecessor;
 	}
 	passedPathVector->insert(passedPathVector->begin(), passedStartVertex);
+}
+
+/**
+ * Returns a random integer from zero to passedUpperBound, inclusive
+ *
+ * Originally written for HW2
+ */
+int getRandomInteger(int passedUpperBound) {
+	return (rand() % (passedUpperBound + 1));
 }
