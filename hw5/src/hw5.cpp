f2a3e3fa11dbba4d74668931d52c616cecb95e20
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-24 11:35:52 -0800

    Implement main

---
+++ hw5/src/hw5.cpp
@@ -0,0 +1,17 @@
+/*********************************************************************
+ * hw5.cpp
+ *
+ * Assignment: hw5
+ *
+ * 11-2017
+ *
+ * Implementation file for CMPS109 HW5: Minimax/Alphabeta AI Algorithm
+ *********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
