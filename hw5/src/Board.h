90f95516f141283c837e6715475d844ae4002928
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-24 13:28:30 -0800

    Update comment header

--- hw5/src/Board.h
+++ hw5/src/Board.h
@@ -4,18 +4,21 @@
  * Assignment: hw5
  *
  * 11-2017
+ *
+ * The Board class is little more than a wrapper around the Graph
+ * class, with some convenience functions thrown in.
  *********************************************************************/
 
 #ifndef BOARD_H_HW5
 #define BOARD_H_HW5
 
-#include "Board.h"
-#include "Graph.h"
-
 #include <iostream>
 #include <limits>
-#include <vector>
 #include <queue>
+#include <vector>
+
+#include "Board.h"
+#include "Graph.h"
 
 using namespace std;
 
@@ -45,12 +48,13 @@ private:
 	int SIZE;
 	int NORTH, SOUTH, EAST, WEST;
 	char COLOR_PLAYER1, COLOR_PLAYER2;
-	Graph<Cell> initBoard(int, int*, int*, int*, int*);
+
+	// Internal functions
+	void initBoard(Graph<Cell>*, int, int*, int*, int*, int*);
 
 public:
-	// Constructor-Destructor
+	// Constructor
 	Board(int, char, char);
-	virtual ~Board();
 
 	// Accessors
 	char getFirstPlayerColor();
@@ -70,10 +74,7 @@ public:
 /**
  * Initializes the main board.
  */
-Graph<Cell> initBoard(int passedDimension, int* passedNorthNode, int* passedSouthNode, int* passedEastNode, int* passedWestNode) {
-	// Instantiate new graph
-	Graph<Cell> newGraph = Graph<Cell>((passedDimension * passedDimension) + 4, false); // Four extra entries for special board-edge vertices
-
+void Board::initBoard(Graph<Cell>* passedGraph, int passedDimension, int* passedNorthNode, int* passedSouthNode, int* passedEastNode, int* passedWestNode) {
 	// Store indices of "special" board-side vertices
 	(*passedNorthNode) = (passedDimension * passedDimension) + 0;
 	(*passedSouthNode) = (passedDimension * passedDimension) + 1;
@@ -85,17 +86,17 @@ Graph<Cell> initBoard(int passedDimension, int* passedNorthNode, int* passedSout
 		// If not the first column, connect to previous index
 		if ((currentIndex % passedDimension) > 0) {
 			int previousIndex = currentIndex - 1;
-			newGraph.addEdge(currentIndex, previousIndex);
+			passedGraph->addEdge(currentIndex, previousIndex);
 		}
 		// If not the first row, connect to top left and top right cells
 		if (currentIndex >= passedDimension) {
 			int topLeft = currentIndex - passedDimension;
-			newGraph.addEdge(currentIndex, topLeft);
+			passedGraph->addEdge(currentIndex, topLeft);
 
 			// If not in the last column, also connect top right cell
 			if ((currentIndex % passedDimension) != (passedDimension - 1)) {
 				int topRight = currentIndex - (passedDimension - 1);
-				newGraph.addEdge(currentIndex, topRight);
+				passedGraph->addEdge(currentIndex, topRight);
 			}
 		}
 	}
@@ -106,20 +107,19 @@ Graph<Cell> initBoard(int passedDimension, int* passedNorthNode, int* passedSout
 		int iteratedSouthEdge = (currentIndex) + (passedDimension * (passedDimension - 1)); // Last row
 		int iteratedEastEdge = (currentIndex * passedDimension) + (passedDimension - 1); // Last index of each row
 		int iteratedWestEdge = (currentIndex * passedDimension); // First index of each row
-		newGraph.addEdge((*passedNorthNode), iteratedNorthEdge);
-		newGraph.addEdge((*passedSouthNode), iteratedSouthEdge);
-		newGraph.addEdge((*passedEastNode), iteratedEastEdge);
-		newGraph.addEdge((*passedWestNode), iteratedWestEdge);
+		passedGraph->addEdge((*passedNorthNode), iteratedNorthEdge);
+		passedGraph->addEdge((*passedSouthNode), iteratedSouthEdge);
+		passedGraph->addEdge((*passedEastNode), iteratedEastEdge);
+		passedGraph->addEdge((*passedWestNode), iteratedWestEdge);
 	}
-	return newGraph;
 }
 
-/* Constructors/Destructors */
-Board::Board(int passedSize, char passedFirstPlayerColor, char passedSecondPlayerColor) {
-	SIZE = passedSize;
-	COLOR_PLAYER1 = passedFirstPlayerColor;
-	COLOR_PLAYER2 = passedSecondPlayerColor;
-	BOARD_REPRESENTATION = initBoard(SIZE, &NORTH, &SOUTH, &EAST, &WEST);
+/* Constructor */
+Board::Board(int passedSize, char passedFirstPlayerColor, char passedSecondPlayerColor) : BOARD_REPRESENTATION((passedSize * passedSize) + 4, false) {
+	Board::initBoard(&(this->BOARD_REPRESENTATION), passedSize, &NORTH, &SOUTH, &EAST, &WEST);
+	this->SIZE = passedSize;
+	this->COLOR_PLAYER1 = passedFirstPlayerColor;
+	this->COLOR_PLAYER2 = passedSecondPlayerColor;
 }
 
 /* Accessors */
@@ -237,15 +237,14 @@ void Board::printBoard(ostream* passedStream) {
 
 	// Print Coordinate Header
 	(*passedStream) << "\t";
-	for (char iteratedRow = 0; iteratedRow < SIZE; iteratedRow += 1) {
-		(*passedStream) << iteratedRow;
-		printString(passedStream, " ", 3);
+	for (char iteratedRow = 'A'; iteratedRow < ('A' + SIZE); iteratedRow += 1) {
+		(*passedStream) << " " << iteratedRow << " ";
 	}
 	(*passedStream) << endl;
 
 	// Print slashes
-	printString(passedStream, " /\\ ", SIZE);
-	printString(passedStream, "/  \\", SIZE);
+	(*passedStream) << "\t";
+	printString(passedStream, " /\\", SIZE);
 	(*passedStream) << endl;
 
 	for (int iteratedRow = 0; iteratedRow < SIZE; iteratedRow += 1) {
@@ -254,7 +253,7 @@ void Board::printBoard(ostream* passedStream) {
 
 		// Print row content
 		printString(passedStream, " ", iteratedRow); // Padding
-		(*passedStream) << "| ";
+		(*passedStream) << "|";
 		for (int iteratedColumn = 0; iteratedColumn < SIZE; iteratedColumn += 1) {
 			int iteratedCell = (iteratedRow * SIZE) + iteratedColumn;
 			char cellColor = BOARD_REPRESENTATION.getVertex(iteratedCell)->color;
@@ -263,11 +262,11 @@ void Board::printBoard(ostream* passedStream) {
 		(*passedStream) << endl;
 
 		// Print slashes
+		(*passedStream) << "\t";
 		printString(passedStream, " ", iteratedRow); // Padding
-		printString(passedStream, "\\  /", SIZE);
-		printString(passedStream, " \\/ ", SIZE);
+		printString(passedStream, " \\/", SIZE);
 		if (iteratedRow != (SIZE - 1)) {
-			(*passedStream) << "\\";
+			(*passedStream) << " \\";
 		}
 		(*passedStream) << endl;
 	}
