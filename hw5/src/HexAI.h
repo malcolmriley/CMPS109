63905622c298aeb5c62972eb30ab516451f3d579
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-24 14:48:34 -0800

    More work towards AI implementation

--- hw5/src/HexAI.h
+++ hw5/src/HexAI.h
@@ -4,20 +4,32 @@
  * Assignment: hw5
  *
  * 11-2017
+ *
+ * Note to grader:
  *********************************************************************/
 
 #ifndef HEXAI_H_HW5
 #define HEXAI_H_HW5
 
+#include <vector>
+
 #include "Board.h"
 
 class HexAI {
 private:
+	// Member Fields
 	Board* BOARD_REFERENCE;
 	int* EVALUATION;
+	char MY_COLOR;
+	char OPPONENT_COLOR;
+	int MY_FIRST_SIDE, MY_SECOND_SIDE, OPONENT_FIRST_SIDE, OPONENT_SECOND_SIDE;
+
+	// Internal Functions
+	void resetEvaluations();
+	void evaluatePaths(vector<int>*, char);
 public:
 	// Constructor / Destructor
-	HexAI(Board*);
+	HexAI(Board*, char, char, int, int, int, int);
 	~HexAI();
 
 	// Miscellaneous
@@ -26,16 +38,62 @@ public:
 };
 
 /* Constructor / Destructor */
-HexAI::HexAI(Board* passedBoardReference) {
+HexAI::HexAI(Board* passedBoardReference, char passedMyColor, char passedOpponentColor, int passedMyFirstSide, int passedMySecondSide, int passedOponentFirstSide, int passedOponentSecondSide) {
 	this->BOARD_REFERENCE = passedBoardReference;
 	this->EVALUATION = new int[this->BOARD_REFERENCE->getCellCount()];
-	for (int ii = 0; ii < this->BOARD_REFERENCE->getCellCount(); ii += 1) {
-		this->EVALUATION[ii] = 0;
-	}
+	this->MY_COLOR = passedMyColor;
+	this->OPPONENT_COLOR = passedOpponentColor;
+	this->MY_FIRST_SIDE = passedMyFirstSide;
+	this->MY_SECOND_SIDE = passedMySecondSide;
+	this->OPONENT_FIRST_SIDE = passedOponentFirstSide;
+	this->OPONENT_SECOND_SIDE = passedOponentSecondSide;
+	this->resetEvaluations();
 }
 
 HexAI::~HexAI() {
 	delete this->EVALUATION;
+}
+
+/* Miscellaneous */
+void HexAI::evaulateBoard() {
+	this->resetEvaluations();
+
+	// Build board representation
+	vector<int> myPieces = vector<int>();
+	vector<int> opponentPieces = vector<int>();
+	for (int ii = 0; ii < this->BOARD_REFERENCE->getCellCount(); ii += 1) {
+		char cellColor = this->BOARD_REFERENCE->getCellColor(ii);
+		if (cellColor != ' ') {
+			if (cellColor == this->MY_COLOR) {
+				myPieces.push_back(ii);
+			}
+			else {
+				opponentPieces.push_back(ii);
+			}
+		}
+	}
+
+	// Evaluate possible opponent paths
+
+	// Evaluate my possible paths
+}
+
+void HexAI::executeMove() {
+	int bestMove = 0;
+	int maxValue = -1;
+	for (int ii = 0; ii < this->BOARD_REFERENCE->getCellCount(); ii += 1) {
+		if (this->EVALUATION[ii] > maxValue) {
+			bestMove = ii;
+		}
+	}
+	this->BOARD_REFERENCE->setCell(this->MY_COLOR, bestMove);
+}
+
+/* Internal Functions */
+void HexAI::resetEvaluations() {
+	for (int ii = 0; ii < this->BOARD_REFERENCE->getCellCount(); ii += 1) {
+		this->EVALUATION[ii] = 0;
+	}
 }
 
 #endif /* HEXAI_H_ */
